d91e7873196c6b1fef5e7e057ea0001d1e089346
pedrolopesdjedjo1 <nodjunta.gb@gmail.com>
2026-09-21 11:00:44 +0000

    Add report routes to the server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,3 @@
-
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes";
@@ -10,6 +9,7 @@ import profileRoutes from "./routes/profileRoutes";
 import searchRoutes from "./routes/searchRoutes";
 import favoriteRoutes from "./routes/favoriteRoutes";
 import adminRoutes from "./routes/adminRoutes";
+import reportRoutes from "./routes/reportRoutes";
 
 dotenv.config();
 
@@ -25,6 +25,7 @@ app.use("/api/profile", profileRoutes);
 app.use("/api/search", searchRoutes);
 app.use("/api/favorites", favoriteRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/reports", reportRoutes);
 
 app.get("/", (_req, res) => {
   res.json({ status: "NôdjuntaAgro GB API rodando" });
