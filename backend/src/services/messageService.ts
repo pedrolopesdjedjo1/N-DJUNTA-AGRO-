179b89175e0d060bf65380ac768a6ce64d18b2c8
pedrolopesdjedjo1 <nodjunta.gb@gmail.com>
2026-09-23 11:49:01 +0000

    Refactor messageService to use shared prisma instance

--- backend/src/services/messageService.ts
+++ backend/src/services/messageService.ts
@@ -1,7 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
+import { prisma } from "../lib/prisma";
 interface SendMessageInput {
   senderId: string;
   receiverId: string;
